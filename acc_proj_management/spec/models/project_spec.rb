f543128844dbc06fd2b016713da3d31734473733
My Pham <mypham1411@gmail.com>
2018-02-25 00:21:41 -0600

    add tests for projects db

--- acc_proj_management/spec/models/project_spec.rb
+++ acc_proj_management/spec/models/project_spec.rb
@@ -2,7 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Project, type: :model do
   it 'is valid with valid fields' do
-    
+  	# Set up
+    project = Project.create(name: 'acc project', description: 'project management',
+    						is_accepted: true, is_finished: false)
+
+    # Exercise
+    name = project.name
+    description = project.description
+    is_accepted = project.is_accepted
+    is_finished = project.is_finished
+
+    # Verify
+    expect(name).to eq 'acc project'
+    expect(description).to eq 'project management'
+    expect(is_accepted).to eq true
+    expect(is_finished).to eq false
   end 
 
   it 'raises ConstraintException' do
