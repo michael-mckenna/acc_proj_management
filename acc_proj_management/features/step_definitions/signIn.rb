6c410ef39188eaec7c28ba665ffe444ef1ef6865
Neal Patel <justnealpatel@gmail.com>
2018-02-28 12:41:21 -0600

    Added instance variable changes to sign up and sign in

--- acc_proj_management/features/step_definitions/signIn.rb
+++ acc_proj_management/features/step_definitions/signIn.rb
@@ -4,11 +4,11 @@
 
 # Step definitions for valid login
 And("I have entered valid username for sign in") do
-    @validLogin.validFormField(currUser, "sign in", "username")
+    @validLogin.validFormField(@currUser, "sign in", "username")
 end
 
 And("I have entered valid password for sign in") do
-    @validLogin.validFormField(currUser, "sign in", "password")
+    @validLogin.validFormField(@currUser, "sign in", "password")
 end
 
 When("I press the sign in button") do
@@ -25,29 +25,29 @@ end
 
 # Step definitions for blank username
 And("I have not entered username for sign in") do
-    @blankUsername.blankFormField(currUser, "sign in", "username")
+    @blankUsername.blankFormField(@currUser, "sign in", "username")
 end
 
 Then("an error about the username should pop up for sign in") do
-    @blankUsername.showFormError(currUser, "sign in", "username")
+    @blankUsername.showFormError(@currUser, "sign in", "username")
 end
 
 # Step definitions for blank email
 And("I have not entered email for sign in") do
-    @blankEmail.blankFormField(currUser, "sign in", "email")
+    @blankEmail.blankFormField(@currUser, "sign in", "email")
 end
 
 Then("an error about the email should pop up for sign in") do
-    @blankEmail.showFormError(currUser, "sign in", "email")
+    @blankEmail.showFormError(@currUser, "sign in", "email")
 end
 
 # Step definitions for blank password
 And("I have not entered password for sign in") do
-    @blankUsername.blankFormField(currUser, "sign in", "password")
+    @blankUsername.blankFormField(@currUser, "sign in", "password")
 end
 
 Then("an error about the password should pop up for sign in") do
-    @blankPassword.showFormError(currUser, "sign in", "password")
+    @blankPassword.showFormError(@currUser, "sign in", "password")
 end
 
 #############################################
@@ -56,27 +56,27 @@ end
 
 # Step definitions for invalid username
 And("I have not entered valid username for sign in") do
-    @invalidUsername.invalidFormField(currUser, "sign in", "username")
+    @invalidUsername.invalidFormField(@currUser, "sign in", "username")
 end
 
 Then("an error about the valid username should pop up for sign in") do
-    @invalidUsername.showFormError(currUser, "invalid username")
+    @invalidUsername.showFormError(@currUser, "invalid username")
 end
 
 # Step definitions for invalid email
 And("I have not entered valid email for sign in") do
-    @invalidEmail.invalidFormField(currUser, "sign in", "email")
+    @invalidEmail.invalidFormField(@currUser, "sign in", "email")
 end
 
 Then("an error about the valid email should pop up for sign in") do
-    @invalidEmail.showFormError(currUser, "invalid email")
+    @invalidEmail.showFormError(@currUser, "invalid email")
 end
 
 # Step definitions for invalid password
 And("I have not entered valid password for sign in") do
-    @invalidPassword.invalidFormField(currUser, "sign in", "password")
+    @invalidPassword.invalidFormField(@currUser, "sign in", "password")
 end
 
 Then("an error about the valid password should pop up for sign in") do
-    @invalidPassword.showFormError(currUser, "invalid password")
+    @invalidPassword.showFormError(@currUser, "invalid password")
 end
--- acc_proj_management/features/step_definitions/signUp.rb
+++ acc_proj_management/features/step_definitions/signUp.rb
@@ -4,23 +4,23 @@
 
 # Step definitions for a valid sign up
 And("I have entered valid first name for sign up") do
-    @validSignUp.validFormField(currUser, "sign up", "first name")
+    @validSignUp.validFormField(@currUser, "sign up", "first name")
 end
 
 And("I have entered valid last name for sign up") do
-    @validSignUp.validFormField(currUser, "sign up", "last name")
+    @validSignUp.validFormField(@currUser, "sign up", "last name")
 end
 
 And("I have entered valid email for sign up") do
-    @validSignUp.validFormField(currUser, "sign up", "email")
+    @validSignUp.validFormField(@currUser, "sign up", "email")
 end
 
 And("I have entered valid password for sign up") do
-    @validSignUp.validFormField(currUser, "sign up", "password")
+    @validSignUp.validFormField(@currUser, "sign up", "password")
 end
 
 And("I have confirmed valid password for sign up") do
-    @validSignUp.confirmed(currUser, "sign up", "password")
+    @validSignUp.confirmed(@currUser, "sign up", "password")
 end
 
 When("I press the sign up button") do
@@ -37,47 +37,47 @@ end
 
 # Step definitions for blank email
 And("I have not entered email for sign up") do
-    @blankEmail.blankFormField(currUser, "sign up", "email")
+    @blankEmail.blankFormField(@currUser, "sign up", "email")
 end
 
 Then("an error about the email should pop up for sign up") do
-    @invalidEmail.showFormError(currUser, "sign up", "blank email")
+    @invalidEmail.showFormError(@currUser, "sign up", "blank email")
 end
 
 # Step definitions for blank first name
 And("I have not entered first name for sign up") do
-    @blankFirstName.blankFormField(currUser, "sign up", "first name")
+    @blankFirstName.blankFormField(@currUser, "sign up", "first name")
 end
 
 Then("an error about the first name should pop up for sign up") do
-    @blankFirstName.showFormError(currUser, "sign up", "blank first name")
+    @blankFirstName.showFormError(@currUser, "sign up", "blank first name")
 end
 
 # Step definitions for blank last name
 And("I have not entered last name for sign up") do
-    @blankLastName.blankFormField(currUser, "sign up", "last name")
+    @blankLastName.blankFormField(@currUser, "sign up", "last name")
 end
 
 Then("an error about the last name should pop up for sign up") do
-    @blankLastName.showFormError(currUser, "sign up", "blank last name")
+    @blankLastName.showFormError(@currUser, "sign up", "blank last name")
 end
 
 # Step definitions for blank password
 And("I have not entered password for sign up") do
-    @blankPassword.blankFormField(currUser, "sign up", "password")
+    @blankPassword.blankFormField(@currUser, "sign up", "password")
 end
 
 Then("an error about the password should pop up for sign up") do
-    @blankPassword.showFormError(currUser, "sign up", "blank password")
+    @blankPassword.showFormError(@currUser, "sign up", "blank password")
 end
 
 # Step definitions for blank password confirmation
 And("I have not entered password confirmation for sign up") do
-    @blankPasswordConf.blankFormField(currUser, "sign up", "password confirmation")
+    @blankPasswordConf.blankFormField(@currUser, "sign up", "password confirmation")
 end
 
 Then("an error about the password confirmation should pop up for sign up") do
-    @blankPasswordConf.showFormError(currUser, "sign up", "blank password confirmation")
+    @blankPasswordConf.showFormError(@currUser, "sign up", "blank password confirmation")
 end
 
 #############################################
@@ -86,18 +86,18 @@ end
 
 # Step definitions for wrong email
 And("I have entered invalid email for sign up") do
-    @invalidEmail.invalidFormField(currUser, "sign up", "email")
+    @invalidEmail.invalidFormField(@currUser, "sign up", "email")
 end
 
 Then("an error about the invalid email should pop up for sign up") do
-    @invalidEmail.showFormError(currUser, "sign up", "invalid email")
+    @invalidEmail.showFormError(@currUser, "sign up", "invalid email")
 end
 
 # Step definitions for wrong password
 And("I have entered invalid password for sign up") do
-    @invalidPassword.invalidFormField(currUser, "sign up", "password")
+    @invalidPassword.invalidFormField(@currUser, "sign up", "password")
 end
 
 Then("an error about the invalid password should pop up for sign up") do
-    @invalidPassword.showFormError(currUser, "sign up", "invalid password")
+    @invalidPassword.showFormError(@currUser, "sign up", "invalid password")
 end
