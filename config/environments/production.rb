f09e5bf0489634746e0b925a6a7c70dc4a148b9e
My Pham <mypham1411@gmail.com>
2018-03-05 23:58:51 -0600

    add config to support UI display in Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Support for UI display in Heroku
+  config.serve_static_assets = true
+  config.assets.compile = true
 end
