459676511633d6eff9a9d688e43d399e0a8f7351
My Pham <mypham1411@gmail.com>
2018-03-05 23:31:46 -0600

    modify some rspec tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,15 +4,31 @@ RSpec.describe SessionsController, type: :controller do
 
   describe "GET #create" do
     it "returns http success" do
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
+        :provider => 'google',
+        :uid => '123545',
+        info: {
+          email: "email@gmail.com",
+          first_name: "User",
+          last_name: "Test"
+        },
+      credentials: {
+        token: "abcdefg12345",
+        refresh_token: "12345abcdefg",
+        expires_at: DateTime.now,
+      }
+      })
+      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google] 
       get :create
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(302)
     end
   end
 
   describe "GET #destroy" do
     it "returns http success" do
       get :destroy
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(302)
     end
   end
 
