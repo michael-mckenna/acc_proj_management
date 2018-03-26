1c23610b2e9ebb730f5b8113d8d7755ea84e78e6
My Pham <mypham1411@gmail.com>
2018-03-26 12:05:00 -0500

    complete add project functionality

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ApplicationRecord
     has_and_belongs_to_many :users
+    validates :name, presence: true, length: {minimum: 3, maximum: 50}
+	validates :description, presence: true, length: {minimum: 10, maximum: 300}
+	validates_uniqueness_of :name
 end
