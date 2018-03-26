6a6717f730e49fe1ae8c7224485fa8d92a1fc138
michael-mckenna <mdmckenna12@gmail.com>
2018-03-26 11:54:18 -0500

    added basis for project controller spec

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,3 +1,28 @@
 RSpec.describe ProjectsController do
+    describe "GET #index" do
+        it "populates an array of projects"
+        it "renders the :index view"
+      end
+      
+      describe "GET #show" do
+        it "assigns the requested project to @projects"
+        it "renders the :show template"
+      end
+      
+      describe "GET #new" do
+        it "assigns a new Project to @project"
+        it "renders the :new template"
+      end
+      
+      describe "POST #create" do
+        context "with valid attributes" do
+          it "saves the new projects in the database"
+          it "redirects to the projects page"
+        end
         
+        context "with invalid attributes" do
+          it "does not save the new projects in the database"
+          it "re-renders the :new template"
+        end
+      end    
 end
