d8584e7d82b852409e0f81ce53d6ca9ab54b02d6
dawsondeere <dawsondeere19@gmail.com>
2018-03-26 16:23:20 -0500

    added tests for new and create actions

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,14 +1,16 @@
 
 RSpec.describe ProjectsController do
     describe "GET #index" do
+        before(:each) do
+            get :index
+        end
+
         it "populates an array of projects" do
             project = FactoryBot.create(:project)
-            get :index
             assigns(:projects).should eq([project])
         end
 
         it "renders the :index view" do 
-            get :index
             response.should render_template :index
         end
     end
@@ -28,19 +30,45 @@ RSpec.describe ProjectsController do
     end
     
     describe "GET #new" do
-        it "assigns a new Project to @project"
-        it "renders the :new template"
+        before(:each) do
+            get :new
+        end
+
+        it "assigns a new Project to @project" do
+	    assigns(:project).id.should eq(nil)
+            assigns(:project).name.should eq(nil)
+            assigns(:project).description.should eq(nil)
+        end
+
+        it "renders the :new template" do
+            response.should render_template :new
+        end
     end
     
     describe "POST #create" do
         context "with valid attributes" do
-            it "saves the new projects in the database"
-            it "redirects to the projects page"
+            it "saves the new project in the database" do
+                project = FactoryBot.create(:project)
+                expect(project.save).to eq true
+            end
+
+            it "redirects to the projects page" do
+                get :index
+                response.should render_template :index
+            end
         end
     
         context "with invalid attributes" do
-            it "does not save the new projects in the database"
-            it "re-renders the :new template"
+            it "does not save the new project in the database" do
+                project = FactoryBot.build(:project)
+                project.name = "a"
+		expect(project.save).to eq false
+            end
+
+            it "re-renders the :new template" do
+                get :new
+                response.should render_template :new
+            end
         end
     end    
 end
