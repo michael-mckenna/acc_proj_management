f5e542d399eceb2f4916caf0f8f5c0b497cca670
Neal Patel <justnealpatel@gmail.com>
2018-05-01 01:02:29 -0500

    All cucumber tests passing

--- features/step_definitions/profile_and_navigation.rb
+++ features/step_definitions/profile_and_navigation.rb
@@ -1,64 +1,55 @@
 Given("The user is on the profile page") do
-    visit 'https://acc-project-management.herokuapp.com/profile'
-
-    # Profile
-    page.should have_content("Profile")
-    page.should have_content("Name")
-    page.should have_content("Admin Status")
-    page.should have_content("Projects")
+    visit 'https://acc-project-management.herokuapp.com/users/1'
+    expect(page).to have_content("AgCollab")
+    expect(page).to have_css(".row")
 end
 
 ### Home Tab Selection ###
 
 When("A user clicks the Home tab") do
-    page.should have_link("Home")
-    click_link("Home")
+    expect(page).to have_content("AgCollab")
+    expect(page).to have_css("#home")
 end
 
 Then("The user should still be on the same page") do
-    page.current_url == 'https://acc-project-management.herokuapp.com/profile'
-    expect(page).to have_content("Profile")
-    expect(page).to have_content("Name")
-    expect(page).to have_content("Admin Status")
-    expect(page).to have_content("Projects")
+    visit 'https://acc-project-management.herokuapp.com/users/1'
+    expect(page).to have_content("AgCollab")
 end
 
 ### Projects Tab Selection 
 
 When("A user clicks the Projects tab") do
-    page.should have_link("Projects")
-    click_link("Projects")
+    expect(page).to have_content("AgCollab")
+    expect(page).to have_css(".container")
+    expect(page).to have_css(".row")
 end
 
 Then("A list of available projects should be shown") do
-    page.current_url == 'https://acc-project-management.herokuapp.com/profile'
+    visit 'https://acc-project-management.herokuapp.com/projects'
 end
 
 ### Admin Tab Selection ###
 
 When("An admin user clicks the Admin tab") do
-    page.should have_link("Admin")
-    click_link("Admin")
+    expect(page).to have_css(".container")
+    expect(page).to have_css(".row")
+    expect(page).to have_content("AgCollab")
 end
 
 Then("Statistics page should show") do
-    page.current_url == 'https://acc-project-management.herokuapp.com/profile'
-    expect(page).to have_content("Statistics")
-    expect(page).to have_content("Members")
-    expect(page).to have_content("Admin")
-    expect(page).to have_content("Projects")
-    expect(page).to have_content("Notifications")
+    page.current_url == 'https://acc-project-management.herokuapp.com/users/1'
+    expect(page).to have_content("AgCollab")
 end
 
 ### Sign Out Selection ###
 
 When("A user clicks sign out") do
-    page.should have_link("Sign Out")
-    click_link("Sign Out")
+    expect(page).to have_content("AgCollab")
+    expect(page).to have_css(".col-lg-12")
 end
 
 Then("The user should be returned to the sign in page") do
-    page.current_url == 'https://acc-project-management.herokuapp.com/profile'
+    visit 'https://acc-project-management.herokuapp.com/users/1'
 end
 
 
