fbe22f35c55f4eb1fa3660d97997f72d0223332c
My Pham <mypham1411@gmail.com>
2018-05-01 18:23:29 -0500

    test domain

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '374048919865-3feem15hba2psnldoharhjbn6ecgqd93.apps.googleusercontent.com', 
-  'Z4OCBruAPN7PG_wZ0tIhqssa', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  'Z4OCBruAPN7PG_wZ0tIhqssa', {hd: 'tamu.edu', client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
